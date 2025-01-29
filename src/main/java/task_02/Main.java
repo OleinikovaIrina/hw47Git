4fadd96966556a01eaf7b20918fc6082886750ed
OleinikovaIrina <irenolgaruda@gmail.com>
2025-01-29 19:48:18 +0100

    Task_02

---
+++ src/main/java/task_02/Main.java
@@ -0,0 +1,10 @@
+package task_02;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Задача 2");
+        Dog dog = new Dog("Reggy", 12);
+    }
+}
