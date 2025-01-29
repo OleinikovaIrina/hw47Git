14fa912c8f491a2a7bbc8df74b5748f5fba55fc1
OleinikovaIrina <irenolgaruda@gmail.com>
2025-01-29 20:25:21 +0100

    Task_02

--- src/main/java/task_01/Main.java
+++ src/main/java/task_01/Main.java
@@ -3,7 +3,7 @@ package task_01;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Первая  задача");
+        System.out.println("Первая  задача  c изменениями");
 
         Cat cat = new Cat("Ball", 5);
     }
